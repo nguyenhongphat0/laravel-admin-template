bb870d76022d6715cc30012ff8131500fc1a8140
Hồng Phát <yhongphat.js@gmail.com>
2021-02-10 12:04:06 +0700

    scaffold project with adminlte

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Repositories\UserRepository;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory(10)->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345678'),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
